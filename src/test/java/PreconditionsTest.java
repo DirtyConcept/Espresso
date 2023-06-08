1b39f71e9efff020a959e3878417b51de86d6d9c
SadGhost <idanr2015@gmail.com>
2023-06-08 13:50:11 +0300

    refractor: improve the unit testing for precond.

---
+++ src/test/java/PreconditionsTest.java
@@ -0,0 +1,44 @@
+import dev.sadghost.ghostingly.base.Preconditions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PreconditionsTest {
+
+    @Test
+    void testCheckNotNullWithNonNullObject() {
+        // Arrange
+        Object obj = new Object();
+
+        // Act and Assert
+        assertDoesNotThrow(() -> Preconditions.checkNonNull(obj));
+    }
+
+    @Test
+    void testCheckNotNullWithNullObject() {
+        // Arrange
+        Object obj = null;
+
+        // Act and Assert
+        assertThrows(NullPointerException.class, () -> Preconditions.checkNonNull(obj));
+    }
+
+    @Test
+    void testCheckArgumentWithTrueCondition() {
+        // Arrange
+        boolean condition = true;
+
+        // Act and Assert
+        assertDoesNotThrow(() -> Preconditions.checkArgument(condition));
+    }
+
+    @Test
+    void testCheckArgumentWithFalseCondition() {
+        // Arrange
+        boolean condition = false;
+
+        // Act and Assert
+        assertThrows(IllegalArgumentException.class, () -> Preconditions.checkArgument(condition));
+    }
+}
