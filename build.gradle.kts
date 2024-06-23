8501bb3c309239c72350c0f93def8d80ccbfa815
SadGhost <idanr2015@gmail.com>
2024-06-24 00:00:49 +0300

    chore: add yaml library and update some libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,28 +5,25 @@ plugins {
 
 repositories {
     mavenLocal()
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
-
-    maven {
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
+    maven("https://repo.maven.apache.org/maven2/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:24.1.0")
+    implementation("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8.4")
 
-    compileOnly("org.mongodb:mongo-java-driver:3.12.14")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    compileOnly("org.mongodb:mongodb-driver-sync:5.1.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
     testCompileOnly("org.jetbrains:annotations:24.1.0")
 }
 
 
 group = "dev.sadghost"
-version = "1.0.0"
+version = "1.0.2"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 publishing {
